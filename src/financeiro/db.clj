e173df45418ba328b9cef0661399e88343c46010
Tatianeb <tatiane.berrocal@gmail.com>
2022-01-11 00:17:20 -0300

    inclusão do saldo

---
+++ src/financeiro/db.clj
@@ -0,0 +1,5 @@
+(ns financeiro.db)
+
+(defn registrar
+  [transacao]
+  transacao)
--- test/unitarios/financeiro/handler_test.clj
+++ test/unitarios/financeiro/handler_test.clj
@@ -2,7 +2,8 @@
   (:require [midje.sweet :refer :all]
             [ring.mock.request :as mock]
             [cheshire.core :as json]
-            [financeiro.handler :refer :all]))
+            [financeiro.handler :refer :all]
+            [financeiro.db :as db]))
 
 (facts "Dá um 'Olá mundo!' na rota raiz"
        (let [response (app (mock/request :get "/"))]
@@ -28,3 +29,18 @@
                (:status response) => 200)
          (fact "O texto do corpo é '0'"
                (:body response) => "{\"saldo\":0}")))
+
+(facts "Registra uma receita no valor de 10"
+       (against-background (db/registrar {:valor 10
+                                          :tipo  "receita"})
+                           => {:id 1 :valor 10 :tipo "receita"})
+
+       (let [response (app (-> (mock/request :post "/transacoes")
+                               (mock/json-body {:valor 10
+                                                :tipo  "receita"})))]
+
+         (fact "o status da resposta é 201"
+               (:status response) => 201)
+
+         (fact "o texto do corpo é um JSON com o conteúdo enviado e um id"
+               (:body response) => "{\"id\":1,\"valor\":10,\"tipo\":\"receita\"}")))
