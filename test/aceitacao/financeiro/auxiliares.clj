cb4a3b11b85a0b5f19d266b793fc117a6342f419
Tatianeb <tatiane.berrocal@gmail.com>
2022-01-10 21:17:22 -0300

    endpoint saldo com testes

---
+++ test/aceitacao/financeiro/auxiliares.clj
@@ -0,0 +1,25 @@
+(ns financeiro.auxiliares
+  (:require    [financeiro.handler :refer [app]]
+               [ring.adapter.jetty :refer [run-jetty]]
+               [clj-http.client :as http]))
+
+(def servidor (atom nil))
+
+(def porta-padrao 3001)
+
+(defn iniciar-servidor [porta]
+  (swap! servidor
+         (fn [_] (run-jetty app {:port porta :join? false}))))
+
+(defn parar-servidor []
+  (.stop @servidor))
+
+(defn endereco-para
+  [rota]
+  (str "http://localhost:" porta-padrao "/saldo"))
+
+(def requisicao-para (comp http/get endereco-para))
+
+(defn conteudo
+  [rota]
+  (:body (requisicao-para rota)))
--- test/unitarios/financeiro/handler_test.clj
+++ test/unitarios/financeiro/handler_test.clj
@@ -1,6 +1,7 @@
 (ns financeiro.handler-test
   (:require [midje.sweet :refer :all]
             [ring.mock.request :as mock]
+            [cheshire.core :as json]
             [financeiro.handler :refer :all]))
 
 (facts "Dá um 'Olá mundo!' na rota raiz"
@@ -20,8 +21,10 @@
                (:body response) => "Recurso não encontrado")))
 
 (facts "O saldo inicial é 0"
+       (against-background (json/generate-string {:saldo 0})
+                           => "{\"saldo\":0}")
        (let [response (app (mock/request :get "/saldo"))]
          (fact "O status da resposta é 200"
                (:status response) => 200)
          (fact "O texto do corpo é '0'"
-               (:body response) => "0")))
+               (:body response) => "{\"saldo\":0}")))
